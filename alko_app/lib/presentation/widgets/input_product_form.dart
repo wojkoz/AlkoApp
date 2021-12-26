51d7493ca0b9015d73a84c8b14b3c37fbfade432
Wojciech Kozioł <wkoziol3ib@gmail.com>
2021-12-26 19:46:43 +0100

    updated flutter, bloc

--- alko_app/lib/presentation/widgets/input_product_form.dart
+++ alko_app/lib/presentation/widgets/input_product_form.dart
@@ -6,6 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:form_builder_validators/form_builder_validators.dart';
 import 'package:intl/intl.dart';
 
 // ignore: must_be_immutable
